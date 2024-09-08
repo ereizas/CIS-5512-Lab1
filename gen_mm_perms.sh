733a522d2ffcc1e2f1afe9877faba7f91abe5b74
ereizas <ereizas@gmail.com>
2024-09-08 13:29:13 -0400

    Redirected output of permutations to txt files

--- gen_mm_perms.sh
+++ gen_mm_perms.sh
@@ -4,34 +4,34 @@ sed -i s/#1/i/g matrix.c
 sed -i s/#2/j/g matrix.c
 sed -i s/#3/k/g matrix.c
 gcc -o matrixijk -DN=5 -DPRINT matrix.c
-./matrixijk
+./matrixijk > matrixijk.txt
 
 sed -i s/#1/i/g matrix.c
 sed -i s/#2/k/g matrix.c
 sed -i s/#3/j/g matrix.c
 gcc -o matrixikj -DN=5 -DPRINT matrix.c
-./matrixikj
+./matrixikj > matrixikj.txt
 
 sed -i s/#1/j/g matrix.c
 sed -i s/#2/i/g matrix.c
 sed -i s/#3/k/g matrix.c
 gcc -o matrixjik -DN=5 -DPRINT matrix.c
-./matrixjik
+./matrixjik > matrixjik.txt
 
 sed -i s/#1/k/g matrix.c
 sed -i s/#2/j/g matrix.c
 sed -i s/#3/i/g matrix.c
 gcc -o matrixkji -DN=5 -DPRINT matrix.c
-./matrixkji
+./matrixkji > matrixkji.txt
 
 sed -i s/#1/k/g matrix.c
 sed -i s/#2/i/g matrix.c
 sed -i s/#3/j/g matrix.c
 gcc -o matrixkij -DN=5 -DPRINT matrix.c
-./matrixkij
+./matrixkij > matrixkij.txt
 
 sed -i s/#1/j/g matrix.c
 sed -i s/#2/k/g matrix.c
 sed -i s/#3/i/g matrix.c
 gcc -o matrixjki -DN=5 -DPRINT matrix.c
-./matrixjki
+./matrixjki > matrixjki.txt
