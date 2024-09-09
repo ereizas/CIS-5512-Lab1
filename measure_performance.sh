029532de400b37135910af89bb0b1040af7baf76
ereizas <ereizas@gmail.com>
2024-09-09 16:24:05 -0400

    Accepted input for n and calculated performance

--- measure_performance.sh
+++ measure_performance.sh
@@ -1,17 +1,13 @@
+n=5
+if [ $# -eq 1 ]; then
+    n=$1
+fi
 results=("matrixijk.txt" "matrixikj.txt" "matrixjik.txt" "matrixjki.txt" "matrixkij.txt" "matrixkji.txt")
-runtimes=()
-./gen_mm_perms.sh 1000
+echo "Matrix multiplication N=$n:"
+./gen_mm_perms.sh $n
 ./compare_mats.sh
 for i in ${!results[@]}; do
-    runtimes[$i]=$(sed -n 's/Time = \([0-9]*\.[0-9]*\)/\1/p' ${results[$i]})
-done
-./gen_mm_perms.sh 2000
-./compare_mats.sh
-for i in ${!results[@]}; do
-    runtimes[$i]=$(sed -n 's/Time = \([0-9]*\.[0-9]*\)/\1/p' ${results[$i]})
-done
-./gen_mm_perms.sh 5000
-./compare_mats.sh
-for i in ${!results[@]}; do
-    runtimes[$i]=$(sed -n 's/Time = \([0-9]*\.[0-9]*\)/\1/p' ${results[$i]})
+    runtime=$(sed -n 's/Time = \([0-9]*\.[0-9]*\)/\1/p' ${results[$i]})
+    performance=$(echo "scale=2; ($n*$n*$n) / $runtime" | bc)
+    echo ${results[$i]}: Runtime=$runtime Performance=$performance
 done
