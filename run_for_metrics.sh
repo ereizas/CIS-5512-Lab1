0ff2521294173afa6a1e23d4cdc5240680e833cd
ereizas <ereizas@gmail.com>
2024-09-10 15:57:06 -0400

    Edited run_for_metrics according to moving of metric calculations

--- run_for_metrics.sh
+++ run_for_metrics.sh
@@ -1,3 +1,6 @@
-./measure_performance.sh 1000
-./measure_performance.sh 2000
-./measure_performance.sh 5000
+./gen_mm_perms.sh 1000
+./compare_mats.sh 1000
+./gen_mm_perms.sh 2000
+./compare_mats.sh 2000
+./gen_mm_perms.sh 5000
+./compare_mats.sh 5000
