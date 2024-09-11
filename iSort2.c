52211946207a22cbb77124727e5fd18e97dcd775
ereizas <ereizas@gmail.com>
2024-09-11 16:48:04 -0400

    Split array into halves, insert sroted, and then merge sorted

--- iSort2.c
+++ iSort2.c
@@ -28,52 +28,85 @@ void display() {
   printf("]\n");
 }
 
-void insertionSort() {
-  struct timeval start, stop;   
-  gettimeofday(&start, 0);
+void insertionSort(int *arr,int size) {
   int valueToInsert;
   int holePosition;
   int i;
  
   // loop through all numbers 
-  for(i = 1; i < MAX; i++) { 
-
+  for(i = 1; i < size; i++) { 
      // select a value to be inserted. 
-     valueToInsert = intArray[i];
+     valueToInsert = arr[i];
 
      // select the hole position where number is to be inserted 
      holePosition = i;
 
      // check if previous no. is larger than value to be inserted 
-     while (holePosition > 0 && intArray[holePosition-1] > valueToInsert) {
-        intArray[holePosition] = intArray[holePosition-1];
+     while (holePosition > 0 && arr[holePosition-1] > valueToInsert) {
+        arr[holePosition] = arr[holePosition-1];
         holePosition--;
-        //printf(" item moved : %d\n" , intArray[holePosition]);
+        //printf(" item moved : %d\n" , arr[holePosition]);
      }
 
      if(holePosition != i) {
         //printf(" item inserted : %d, at position : %d\n" , valueToInsert,holePosition);
         // insert the number at current hole
-        intArray[holePosition] = valueToInsert;
+        arr[holePosition] = valueToInsert;
      }
 
      //printf("Iteration %d#:",i);
      //display();
 
   } 
-  gettimeofday(&stop, 0);
-  printf("Time = %.6f\n\n",(stop.tv_sec + stop.tv_usec*1e-6) - (start.tv_sec + start.tv_usec*1e-6));
+}
+
+int mergeSort(int *arr1, int *arr2){
+   int i = 0, l = 0, r = 0;
+   while(l<MAX/2 && r<MAX/2+MAX%2){
+      if(arr1[l]<arr2[r]){
+         intArray[i]=arr1[l];
+         l++;
+      }
+      else{
+         intArray[i]=arr2[r];
+         r++;
+      }
+      i++;
+   }
+   while(l<MAX/2){
+      intArray[i]=arr1[l];
+      l++;
+      i++;
+   }
+   while(r<MAX/2+MAX%2){
+      intArray[i]=arr2[r];
+      r++;
+      i++;
+   }
 }
 
 void main() {
-    for(int i=0; i<MAX; i++){
-        intArray[i]=rand()%MAX;
-    }
-  printf("Input Array: ");
-  display();
-  printline(50);
-  insertionSort();
-  printf("Output Array: ");
-  display();
-  printline(50);
+   for(int i=0; i<MAX; i++){
+      intArray[i]=rand()%MAX;
+   }
+   printf("Input Array: ");
+   display();
+   printline(50);
+   struct timeval start, stop;   
+   gettimeofday(&start, 0);
+   int arr1[MAX/2], arr2[MAX/2+MAX%2];
+   for(int i = 0;i<MAX/2;i++){
+      arr1[i]=intArray[i];
+   }
+   for(int i = MAX/2;i<MAX+MAX%2;i++){
+      arr2[i-MAX/2]=intArray[i];
+   }
+   insertionSort(arr1,MAX/2);
+   insertionSort(arr2,MAX/2+MAX%2);
+   mergeSort(arr1,arr2);
+   gettimeofday(&stop, 0);
+   printf("Time = %.6f\n\n",(stop.tv_sec + stop.tv_usec*1e-6) - (start.tv_sec + start.tv_usec*1e-6));
+   printf("Output Array: ");
+   display();
+   printline(50);
 }
