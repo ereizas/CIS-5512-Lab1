fe761e3f753056fcdd97a8145e21b16c8aaa77ee
Eric Reizas <71237683+ereizas@users.noreply.github.com>
2024-09-12 19:38:30 -0400

    Fixed syntax issues

--- run_for_metrics.sh
+++ run_for_metrics.sh
@@ -1,15 +1,16 @@
+
 #/bin/bash
 start=100
 stop=3000
 step=100
 if [ $# -eq 3 ]; then
-    start=$1
-    stop=$2
-    step=$3
+        start=($1)
+        stop=($2)
+        step=($3)
 fi
-while start<=stop
+while(($start<=$stop))
 do
-  ./gen_mm_perms.sh start
-  ./compare_mats.sh start
-  start=start+step
+  ./gen_mm_perms.sh $start
+  ./compare_mats.sh $start
+  start=$(( start+step ))
 done
